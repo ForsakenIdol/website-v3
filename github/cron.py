8a83054688dec934c79e3d96cf2b7185c5e48b5f
lachldwaws <lachldw@amazon.com>
2022-05-01 10:17:37 +0800

    Added simple github python script

---
+++ github/cron.py
@@ -0,0 +1,9 @@
+from github import Github as GitHub_Initializer
+import os
+
+# Requires the user's personal access token in an environment variable named "GITHUB_PAN"
+g = GitHub_Initializer(os.environ["GITHUB_PAN"])
+
+# Test to see if this works
+for repo in g.get_user().get_repos():
+    print(repo.name)
