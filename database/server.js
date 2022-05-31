f18cd41fdf0f18dada58e43c733f0657e4f7c425
lachldwaws <lachldw@amazon.com>
2022-05-31 14:53:20 +0800

    Updated server script to use the website table

--- database/server.js
+++ database/server.js
@@ -4,14 +4,14 @@ const { Client } = require('pg');
 const client = new Client({
     user: 'postgres',
     host: 'localhost',
-    database: 'dvdrental',
+    database: 'website',
     // The password goes in an environment variable in production hosting.
     port: 5432
 });
 
 client.connect();
 
-client.query('SELECT store_id, last_update FROM store', (err, res) => {
+client.query('SELECT * FROM github;', (err, res) => {
     console.log(err ? err.stack : res.rows)
     client.end()
   })
