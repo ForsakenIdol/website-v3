bdd0c59abfb4dd6264b5118c15d099a537d80b26
lachldwaws <lachldw@amazon.com>
2022-07-26 14:09:09 +0800

    Wrapped middleware in Express.js application

--- middleware/server.js
+++ middleware/server.js
@@ -1,6 +1,20 @@
-// Do not run this file. It is purely used for syntax checking before passing to the "middleware.yaml" configmap.
-// The 'pg' library uses the PGPASSWORD environment variable if the password is not specified in the script.
+/*
+ ******************************
+ * PostgreSQL Server
+ ******************************
+ * 
+ * This server contains the 4 main CRUD operations all targeting the 'github' table 'website' database.
+ * It assumes that the relevant relations exist and makes no atttempt to check that they do.
+ * 
+ * Run this file with "npm run dev". The relevant environment variables are as follows.
+ * - PGHOST: PostgreSQL hostname.
+ * - PGPASSWORD: PostgreSQL password.
+ * - EXPRESSPORT: Port for the Express.js wrapper.
+ * 
+ */
+
 const { Client } = require('pg');
+const express = require('express');
 
 const client = new Client({
     // The host and password are platform-dependent and so we pass these as environment variables.
@@ -9,9 +23,40 @@ const client = new Client({
     port: 5432
 });
 
-client.connect();
+const app = express();
+const app_port = process.env.EXPRESSPORT;
 
-client.query('SELECT * FROM github;', (err, res) => {
-    console.log(err ? err.stack : res.rows)
+app.listen(app_port, () => {
+    console.log(`Server listening on port ${app_port}.`);
+    client.connect(err => {
+        if (err) {
+            console.log(err);
+            throw "There was an error connecting to the PostgreSQL database.";
+        }
+        else console.log(`Connected to the PostgreSQL database at '${process.env.PGHOST}'.`);
+    });
+});
+
+/* Test Paths */
+
+app.get('/test', (req, res) => {
+    client.query('SELECT * FROM github;', (err, client_res) => {
+        return res.send(err ? err.stack : client_res.rows);
+    });
+});
+
+app.get('/kill', (req, res) => {
     client.end()
-  });
+    .then(() => {
+        console.log("Successfully disconnected client.");
+        res.send("Successfully disconnected client.");
+        process.exit(0);
+    })
+    .catch(error => {
+        console.log("Error during disconnection.");
+        res.send("Error during disconnection.");
+        process.exit(1);
+    });
+})
+
+  /* CRUD Paths */
